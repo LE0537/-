557e0aa33d35bc88952a60c79f09049c66268c08
LE0537 <108453310+LE0537@users.noreply.github.com>
2022-10-12 19:22:12 +0900

    1
    1

---
+++ Client/Private/Terrain.cpp
@@ -0,0 +1,142 @@
+#include "stdafx.h"
+#include "..\Public\Terrain.h"
+
+#include "GameInstance.h"
+
+CTerrain::CTerrain(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+	: CGameObject(pDevice, pContext)
+{
+}
+
+CTerrain::CTerrain(const CTerrain & rhs)
+	: CGameObject(rhs)
+{
+}
+
+HRESULT CTerrain::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CTerrain::Initialize(void * pArg)
+{
+	if (FAILED(Ready_Components()))
+		return E_FAIL;
+	
+	return S_OK;
+}
+
+void CTerrain::Tick(_float fTimeDelta)
+{
+
+}
+
+void CTerrain::Late_Tick(_float fTimeDelta)
+{
+	if (nullptr != m_pRendererCom)
+		m_pRendererCom->Add_RenderGroup(CRenderer::RENDER_NONALPHABLEND, this);
+}
+
+HRESULT CTerrain::Render()
+{
+	if (nullptr == m_pShaderCom ||
+		nullptr == m_pVIBufferCom)
+		return E_FAIL;
+
+	if (FAILED(SetUp_ShaderResources()))
+		return E_FAIL;
+
+	m_pShaderCom->Begin();
+
+	m_pVIBufferCom->Render();
+
+	return S_OK;
+}
+
+HRESULT CTerrain::Ready_Components()
+{
+	/* For.Com_Renderer */
+	if (FAILED(__super::Add_Components(TEXT("Com_Renderer"), LEVEL_STATIC, TEXT("Prototype_Component_Renderer"), (CComponent**)&m_pRendererCom)))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+	if (FAILED(__super::Add_Components(TEXT("Com_Transform"), LEVEL_STATIC, TEXT("Prototype_Component_Transform"), (CComponent**)&m_pTransformCom)))
+		return E_FAIL;
+
+	/* For.Com_Shader */
+	if (FAILED(__super::Add_Components(TEXT("Com_Shader"), LEVEL_GAMEPLAY, TEXT("Prototype_Component_Shader_VtxNorTex"), (CComponent**)&m_pShaderCom)))
+		return E_FAIL;
+
+	/* For.Com_Texture */
+	if (FAILED(__super::Add_Components(TEXT("Com_Texture"), LEVEL_GAMEPLAY, TEXT("Prototype_Component_Texture_Terrain"), (CComponent**)&m_pTextureCom)))
+		return E_FAIL;
+
+	/* For.Com_VIBuffer */
+	if (FAILED(__super::Add_Components(TEXT("Com_VIBuffer"), LEVEL_GAMEPLAY, TEXT("Prototype_Component_VIBuffer_Terrain"), (CComponent**)&m_pVIBufferCom)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CTerrain::SetUp_ShaderResources()
+{
+	if (nullptr == m_pShaderCom)
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->Set_RawValue("g_WorldMatrix", &m_pTransformCom->Get_World4x4_TP(), sizeof(_float4x4))))
+		return E_FAIL;
+
+	CGameInstance*		pGameInstance = CGameInstance::Get_Instance();
+	Safe_AddRef(pGameInstance);
+	
+	if (FAILED(m_pShaderCom->Set_RawValue("g_ViewMatrix", &pGameInstance->Get_TransformFloat4x4_TP(CPipeLine::D3DTS_VIEW), sizeof(_float4x4))))
+		return E_FAIL;
+	
+	if (FAILED(m_pShaderCom->Set_RawValue("g_ProjMatrix", &pGameInstance->Get_TransformFloat4x4_TP(CPipeLine::D3DTS_PROJ), sizeof(_float4x4))))
+		return E_FAIL;
+
+	Safe_Release(pGameInstance);
+
+	if (FAILED(m_pShaderCom->Set_ShaderResourceView("g_DiffuseTexture", m_pTextureCom->Get_SRV(0))))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+CTerrain * CTerrain::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+{
+	CTerrain*	pInstance = new CTerrain(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		ERR_MSG(TEXT("Failed to Created : CTerrain"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+
+CGameObject * CTerrain::Clone(void * pArg)
+{
+	CTerrain*	pInstance = new CTerrain(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		ERR_MSG(TEXT("Failed to Cloned : CTerrain"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CTerrain::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pTransformCom);
+	Safe_Release(m_pShaderCom);
+	Safe_Release(m_pTextureCom);
+	Safe_Release(m_pVIBufferCom);
+	Safe_Release(m_pRendererCom);
+}
